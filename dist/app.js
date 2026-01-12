480a1de353b87f5118d3345d515210e87d517496
Saim Al Ifran <phpmysql811@gmail.com>
2026-01-12 11:23:41 +0600

    docs(swagger): add complete API documentation for notice module

--- dist/app.js
+++ dist/app.js
@@ -9,12 +9,18 @@ const helmet_1 = __importDefault(require("helmet"));
 const compression_1 = __importDefault(require("compression"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const path_1 = __importDefault(require("path"));
+const yamljs_1 = __importDefault(require("yamljs"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const secret_1 = require("./secret");
 const defaultError_1 = __importDefault(require("./utils/errors/defaultError"));
 const index_1 = __importDefault(require("./routes/index"));
+// Load Swagger documentation
+const swaggerDoc = yamljs_1.default.load(path_1.default.join(__dirname, './docs/swagger.yaml'));
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDoc));
 if (secret_1.nodeEnv !== 'production') {
     app.use((0, morgan_1.default)('dev'));
 }
--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,22 @@ import helmet from 'helmet';
 import compression from 'compression';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import YAML from 'yamljs';
+import swaggerUI from 'swagger-ui-express';
 import { nodeEnv } from './secret';
 import errorHandler from './utils/errors/defaultError';
 import router from './routes/index';
+
+// Load Swagger documentation
+const swaggerDoc = YAML.load(path.join(__dirname, './docs/swagger.yaml'));
 
 const app = express();
  
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
 if(nodeEnv !== 'production'){
     app.use(morgan('dev'));
 }
